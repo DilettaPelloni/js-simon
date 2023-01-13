a80ac7761ce1ae70916eb13d7d4b5603dc28465e
Diletta Pelloni <dilpel@live.it>
2023-01-13 14:01:45 +0100

    impostato timer

--- js/scripts.js
+++ js/scripts.js
@@ -13,16 +13,26 @@ function randomNum(min, max) {
 
 //--------------------------------------------------------------
 
+//vado a prendere la number box
+const numberBox = document.getElementById('number-box');
+
 //mostro 5 numeri casuali
 for (let i = 1; i <= 5; i++) {
     //creo uno span
     const span = createSpan();
-    //ci metto dento un numero casuale da 1 a 100
-    span.innerText = randomNum(1, 100);
-    //vado a prendere la number box
-    const numberBox = document.getElementById('number-box');
+    //ci metto dento un numero casuale da 1 a 10
+    span.innerText = randomNum(1, 10);
     //metto lo span nella number box
     numberBox.append(span);
 }
+
+//timer di 30 secondi
+setTimeout (function() {
+    //rendo invisibile la numebr box
+    numberBox.classList.add('invisible')
+
+}, 2000);
+
+
 
 
