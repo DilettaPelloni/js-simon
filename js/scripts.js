2e2fb9ecd52aa5c54e974a0b549982ed81c7dce4
Diletta Pelloni <dilpel@live.it>
2023-01-13 16:00:21 +0100

    gioco funzionanate

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,40 @@ function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+//restituisci quanti e quali elementi hanno in comune due array (conta anche la posizione)
+function sameElementArray(array1, array2) {
+    //controllo
+    if (array1.length != array2.length) {
+        console.log('Gli array da confrontare devono avere lo stesso numero di elementi!');
+        return false;
+    }
+    //eseguo
+    //creo un contatore
+    let counter = 0;
+    //creo un array per salvare i numeri in comune
+    const sameElements = [];
+    //confronto gli elementi dei due array e mi segno quando corrispondono
+    for (let i = 0; i < array1.length; i++) {
+        if (array1[i] == array2[i]) {
+            counter++;
+            sameElements.push(array1[i]);
+        }
+    }
+    //creo un array col risultato da restituire sotto forma di array
+    const result = [counter, sameElements];
+    return result;
+}
+
+
 //--------------------------------------------------------------
 
 //vado a prendere la number box
 const numberBox = document.getElementById('number-box');
 
-//mostro 5 numeri casuali
+//creo un array in cui inserire i numeri da memorizzare
+const gameArray = [];
+
+//mostro 5 numeri casuali e li salvo in un array
 for (let i = 1; i <= 5; i++) {
     //creo uno span
     const span = createSpan();
@@ -24,14 +52,53 @@ for (let i = 1; i <= 5; i++) {
     span.innerText = randomNum(1, 10);
     //metto lo span nella number box
     numberBox.append(span);
+    //metto il contenuto della span nell'array
+    gameArray.push(span.innerText);
 }
 
-//timer di 30 secondi
+//dopo 30 secondi nascondo la number box
 setTimeout (function() {
     //rendo invisibile la numebr box
-    numberBox.classList.add('invisible')
+    numberBox.classList.add('invisible');
+
+    //dopo 1 secondo chiedo i 5 numeri
+    setTimeout (function() {
+        //creo un array in cui inserire i numeri
+        const inputArray = [];
+    
+        //chiedo all'utente i cinque numeri e li inserisco nell'array
+        for (let i = 1; i <= 5; i++) {
+            const numberInput = prompt('inserisci il ' + i + '° numero:');
+            inputArray.push(numberInput);
+        }
+    
+        //comunico quanti numeri sono stati indovinati
+        console.log('Hai indovinato ' + sameElementArray(inputArray, gameArray)[0] + ' numeri su 5!');
+        //ricordo all'utente quali erano i numeri da indovinare
+        console.log('In numeri da memorizzare erano: ' + gameArray);
+
+        //se ha invovinato qualcosa
+        if (sameElementArray(inputArray, gameArray)[1].length != 0 ) {
+            console.log('I numeri che hai ricordato correttamente sono: ' + sameElementArray(inputArray, gameArray)[1]);
+            if (sameElementArray(inputArray, gameArray)[1].length == gameArray.length) {
+                //se hai indovinato tutto
+                console.log('Hai vinto!!');
+            }
+            else {
+                console.log('Potevi fare di meglio...');
+            }
+        }
+        else {
+            console.log('Non hai memorizzato neanche un numero. Hai perso!');
+        }
+    }, 1000);
 
 }, 2000);
+
+
+
+
+
 
 
 
